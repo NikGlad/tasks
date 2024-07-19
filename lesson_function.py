d627053b58148eb71bbff8e9741277516d309ef5
NikGlad <90272508+NikGlad@users.noreply.github.com>
2024-07-19 21:30:11 +0300

    19_07

--- lesson_function.py
+++ lesson_function.py
@@ -27,7 +27,85 @@
 #     return result
 # print(pow(4, 6))
 
-def pow(number_1, number_2=2):
-    result = number_1 ** number_2
-    return result
-print(pow(4))
+# def pow(number_1=2, number_2=5):
+#     result = number_1 ** number_2
+#     return result
+# print(pow(6))
+
+# def pow(number_1, number_2=2):
+#     result = number_1 ** number_2
+#     print(result)
+#
+# res = pow(6)
+# print(res)
+
+# def pow(number_1, number_2=5):
+#     result = number_1 ** number_2
+#     return
+# print(pow(6))
+
+# def pow(number_1, number_2=5):
+#     pass
+
+# salary = 100000
+# bonus =  50000
+#
+# def info():
+#     print(salary + bonus)
+#
+# info()
+
+
+# salary = 100000
+# bonus =  50000
+#
+# def info():
+#     bonus = 20000
+#     print(salary + bonus)
+#
+# info()
+
+
+# salary = 100000
+# bonus =  50000
+#
+# def info():
+#     salary = 20000
+#     bonus = 20000
+#     some_number = 1
+#     print(salary + bonus)
+#
+# info()
+#
+# print(some_number)
+
+
+# def info():
+#     global some_number
+#     some_number = 1
+#
+# info()
+#
+# print(some_number)
+
+# salary = 100000
+# bonus =  50000
+#
+# def info():
+#     global salary
+#     salary = 20000
+#
+# print(salary)
+# info()
+# print(salary)
+
+
+# my_func = lambda x, y: x**y
+#
+# print(my_func(6, 7))
+
+
+
+# print(list(map(lambda x: (x**2, x**3), [252, 547, 88])))
+
+
