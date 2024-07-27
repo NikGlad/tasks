3cfa8d9e1f006293f51954cad231ada869cb044e
NikGlad <90272508+NikGlad@users.noreply.github.com>
2024-07-27 18:08:10 +0300

    27.07

--- lesson_function.py
+++ lesson_function.py
@@ -118,12 +118,12 @@ students_list = [
     {"name": "Роман", "surname": "Золотарев", "gender": "м", "program_exp": False, "grade": [8, 9, 9, 9], "exam": 6},
 ]
 
-# def get_grades(name, surname, students):
-#     for student in students:
-#         if student['name'] == name and student['surname'] == surname:
-#             print(', '.join(map(str, student['grade'])))
-#
-# get_grades('Иван', 'Васильев', students_list)
+def get_grades(name, surname, students):
+    for student in students:
+        if student['name'] == name and student['surname'] == surname:
+            print(', '.join(map(str, student['grade'])))
+
+get_grades('Иван', 'Васильев', students_list)
 
 
 
@@ -136,3 +136,58 @@ students_list = [
 # b = input('введите фамилию ')
 #
 # get_grades(a, b, students_list)
+
+
+# def get_aw_hw_grade(students):
+#     sum_hw = 0
+#     for student in students:
+#         sum_hw += sum(student['grade'])/len(student['grade'])
+#     return sum_hw / len(students)
+#
+# print(get_aw_hw_grade(students_list))
+
+
+# def get_aw_hw_grade(students, exp):
+#     sum_hw = 0
+#     counter = 0
+#     for student in students:
+#         if student['program_exp'] == exp:
+#             sum_hw += sum(student['grade'])/len(student['grade'])
+#             counter += 1
+#     return sum_hw / counter
+#
+# print(get_aw_hw_grade(students_list, True))
+# print(get_aw_hw_grade(students_list, False))
+#
+def get_aw_hw_grade(students, exp=None):
+    sum_hw = 0
+    counter = 0
+    for student in students:
+        if student['program_exp'] ==exp or exp is None:
+            sum_hw += sum(student['grade'])/len(student['grade'])
+            counter += 1
+    return sum_hw / counter
+
+print(get_aw_hw_grade(students_list))
+print(get_aw_hw_grade(students_list, True))
+print(get_aw_hw_grade(students_list, False))
+
+
+def main(students):
+    while True:
+        comand = input('Введите команду ')
+        if comand == '1':
+            get_grades('Иван', 'Васильев', students)
+        elif comand == '2':
+            print(get_aw_hw_grade(students_list))
+        elif comand == '3':
+            print(get_aw_hw_grade(students_list, True))
+        elif comand == 'q':
+            print('Выход из програмы ')
+            break
+
+main(students_list)
+
+
+
+
